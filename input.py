59667579fd8547b8af70a11d0272b23f0a7daaa2
fongradnastya <fongradnastya@gmail.com>
2022-11-20 19:58:20 +0700

    Started colored output

--- input.py
+++ input.py
@@ -1,4 +1,5 @@
 import argparse
+from colorama import init, Back, Style
 from search import search
 
 
@@ -7,7 +8,7 @@ def main():
     string = None
     keywords = None
     parser = argparse.ArgumentParser(description="collection of parameters")
-    while command != "4":
+    while command != "5":
         print_menu()
         command = input("Your command: ")
         if command == "1":
@@ -15,14 +16,17 @@ def main():
         elif command == "2":
             keywords = input_keywords()
             print(keywords)
-        elif command == "3":
+        elif command == "3" or command == "4":
             string = get_string(string)
             is_correct = False
             if string:
                 keywords = get_keys(keywords)
                 if keywords:
-                    answer = search(string, keywords)
-                    print(answer)
+                    if command == "3":
+                        answer = search(string, keywords)
+                    else:
+                        answer = advanced_search(string, keywords)
+                    print_text(string, answer, keywords)
                     is_correct = True
             if not is_correct:
                 print("Impossible to search data")
@@ -68,10 +72,11 @@ def get_keys(keys):
 
 def print_menu():
     print("------------------------MENU------------------------")
-    print("1 - input text")
+    print("1 - input a text")
     print("2 - add keywords")
-    print("3 - search keywords in text")
-    print("4 - exit")
+    print("3 - default search keywords in the text")
+    print("4 - advanced search keywords in the text")
+    print("5 - exit")
     print("----------------------------------------------------")
 
 
@@ -111,6 +116,72 @@ def input_keywords():
     elif len(keywords) == 1:
         return keywords[0]
     return tuple(keywords)
+
+
+def advanced_search(string, keywords):
+    case_sensitivity = input("Case sensitivity (True/False): ")
+    while case_sensitivity not in ("True", "False"):
+        print("Wrong value, please, try again")
+        case_sensitivity = input("Case sensitivity (True/False): ")
+    if case_sensitivity == "True":
+        case_sensitivity = True
+    else:
+        case_sensitivity = False
+    method = input("Search method (first/last): ")
+    while method not in ("first", "last"):
+        print("Wrong value, please, try again")
+        method = input("Search method (first/last): ")
+    is_correct = False
+    count = 0
+    while not is_correct:
+        count = input("Number of searched substrings: ")
+        if not count.isnumeric():
+            print("Value should be numeric")
+        elif int(count) < 1:
+            print("Value should be positive")
+        else:
+            is_correct = True
+            count = int(count)
+    return search(string, keywords, case_sensitivity, method, count)
+
+
+def print_text(string, found, key):
+    init()
+    colors = (
+        Back.BLACK,
+        Back.RED,
+        Back.BLUE,
+        Back.CYAN,
+        Back.GREEN,
+        Back.MAGENTA,
+        Back.YELLOW,
+    )
+    counter = 0
+    for i, char in enumerate(string):
+        if i in found:
+            print(colors[counter] + char, end="")
+        else:
+            for f in found:
+                if (i > f) and (i < f + len(key)):
+                    print(colors[counter] + char, end="")
+                    break
+                elif i == f + len(key):
+                    counter += 1
+                    print(Back.RESET + char, end="")
+                    break
+                else:
+                    print(Back.RESET + char, end="")
+        if counter >= len(colors):
+            counter = 0
+    print(Back.RESET)
+
+
+def print_one_key():
+    pass
+
+
+def print_many_keys():
+    pass
 
 
 main()
