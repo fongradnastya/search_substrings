2b10b04317c8af0577130ac43485f82862d8294e
fongradnastya <fongradnastya@gmail.com>
2022-11-28 16:39:10 +0700

    Added classes

--- new_search.py
+++ new_search.py
@@ -1,14 +1,39 @@
 from collections import deque
 
-AdjList = []
-
 
 class TreeItem:
-    def __init__(self):
-        self.value = None
-        self.next_states = []
-        self.fail_state = 0
-        self.output = []
+    def __init__(self, value=None, next_states=None, fail_state=0, output=None):
+        self.value = value
+        if not next_states:
+            self.next_states = []
+        else:
+            self.next_states = next_states
+        self.fail_state = fail_state
+        if not output:
+            self.output = []
+        else:
+            self.output = output
+
+    def add_next_state(self, state: int):
+        self.next_states.append(state)
+
+    def get_states(self):
+        return self.next_states
+
+    def add_output(self, keyword):
+        self.output.append(keyword)
+
+    def get_output(self):
+        return self.output
+
+    def set_output(self, output):
+        self.output = output
+
+    def set_fail_state(self, fail_state):
+        self.fail_state = fail_state
+
+    def get_fail_state(self):
+        return self.fail_state
 
 
 class Tree:
@@ -16,6 +41,16 @@ class Tree:
         self.items = []
         item = TreeItem()
         self.items.append(item)
+
+    def add_item(self, value, next_states=None, output=None, fail_state=0):
+        item = TreeItem(value, next_states, fail_state, output)
+        self.items.append(item)
+
+    def __len__(self):
+        return len(self.items)
+
+    def get_item(self, item_id):
+        return self.items[item_id]
 
     def add_keywords(self, keywords):
         """ add all keywords in list of keywords """
@@ -32,39 +67,34 @@ class Tree:
                 else:
                     break
             for i in range(j, len(keyword)):
-                node = {'value': keyword[i], 'next_states': [],
-                        'fail_state': 0,
-                        'output': []}
-                AdjList.append(node)
-                AdjList[current_state]["next_states"].append(len(AdjList) - 1)
-                current_state = len(AdjList) - 1
-            AdjList[current_state]["output"].append(keyword)
+                self.add_item(keyword[i])
+                self.items[current_state].add_next_state(len(self.items) - 1)
+                current_state = len(self.items) - 1
+            self.items[current_state].add_output(keyword)
 
     def set_fail_transitions(self):
         q = deque()
-        child = 0
-        for node in AdjList[0]["next_states"]:
+        for node in self.items[0].get_states():
             q.append(node)
-            AdjList[node]["fail_state"] = 0
+            self.items[node].set_fail_state(0)
         while q:
             r = q.popleft()
-            for child in AdjList[r]["next_states"]:
+            for child in self.items[r].get_states():
                 q.append(child)
-                state = AdjList[r]["fail_state"]
-                while (not self.find_next_state(
-                        state, AdjList[child]["value"])) and state != 0:
-                    state = AdjList[state]["fail_state"]
-                AdjList[child]["fail_state"] = \
-                    self.find_next_state(state, AdjList[child]["value"])
-                if AdjList[child]["fail_state"] is None:
-                    AdjList[child]["fail_state"] = 0
-                AdjList[child]["output"] = \
-                    AdjList[child]["output"] + \
-                    AdjList[AdjList[child]["fail_state"]]["output"]
+                state = self.items[r].get_fail_state()
+                while (not self.find_next_state(state, self.items[child].value)) and state != 0:
+                    state = self.items[state].get_fail_state()
+                self.items[child].set_fail_state(self.find_next_state(state, self.items[child].value))
+                if self.items[child].get_fail_state() is None:
+                    self.items[child].set_fail_state(0)
+                id_n = self.items[child].get_fail_state()
+                output = self.items[child].get_output() + \
+                         self.items[id_n].get_output()
+                self.items[child].set_output(output)
 
     def find_next_state(self, current_state, value):
-        for node in AdjList[current_state]["next_states"]:
-            if AdjList[node]["value"] == value:
+        for node in self.items[current_state].get_states():
+            if self.items[node].value == value:
                 return node
         return None
 
@@ -79,14 +109,14 @@ def get_keywords_found(line, keywords):
     for i in range(len(line)):
         while tree.find_next_state(current_state, line[i]) is None \
                 and current_state != 0:
-            current_state = AdjList[current_state]["fail_state"]
+            current_state = tree.get_item(current_state).get_fail_state()
         current_state = tree.find_next_state(current_state, line[i])
         if current_state is None:
             current_state = 0
         else:
-            for j in AdjList[current_state]["output"]:
+            for j in tree.get_item(current_state).get_output():
                 keywords_found.append({"index": i - len(j) + 1, "word": j})
     return keywords_found
 
 
-print(get_keywords_found("casheweww"))
+print(get_keywords_found("casheweww", ['cash', 'shew', 'ew']))
